ab4342b4e7d71551b5bfa60fb8ba6bf6467ce812
umasuo <liuquan89@gmail.com>
2017-03-11 11:09:43 +0800

    add model

---
+++ src/main/java/com/umasuo/dataprocessor/domain/model/DataProcessor.java
@@ -0,0 +1,70 @@
+package com.umasuo.dataprocessor.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.ZonedDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by umasuo on 17/3/11.
+ * Data processor relations between
+ */
+@Entity
+@Table(name = "data_processor")
+@Setter
+@Getter
+public class DataProcessor {
+
+  @Id
+  @GeneratedValue(generator = "uuid")
+  @GenericGenerator(name = "uuid", strategy = "uuid2")
+  @Column(name = "id")
+  private String id;
+
+  /**
+   * The Created at.
+   */
+  @CreatedDate
+  private ZonedDateTime createdAt;
+
+  /**
+   * The Last modified at.
+   */
+  @LastModifiedDate
+  private ZonedDateTime lastModifiedAt;
+
+  /**
+   * version used for update date check.
+   */
+  private Integer version;
+
+  /**
+   * the input data definition ID of the processor. so the input data can know the data structure
+   * it processed.
+   */
+  private String inputDataDefinitionId;
+
+  /**
+   * the out put data definition, define the out data structure.
+   */
+  private String outputDataDefinitionId;
+
+  /**
+   * the data processor script.
+   */
+  private String processorFileId;
+
+  /**
+   * define when and how to trigger this processor.
+   */
+  private String trigger;
+}
